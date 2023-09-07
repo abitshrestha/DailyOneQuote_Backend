14586f9f5a35fdc306e219fcb1ff978cedf4f5e6
abitshrestha <141226473+abitshrestha@users.noreply.github.com>
2023-09-07 19:06:25 +0545

    Add files via upload

---
+++ routes/quoteroute.js
@@ -0,0 +1,21 @@
+const express=require('express');
+const quoteRouter=express.Router();
+const request=require('request');
+require('dotenv').config();
+const key=process.env.key;
+const URL=process.env.url;
+
+quoteRouter.get('/',(req,res)=>{
+    request.get({
+        url:URL,
+        headers:{
+            'X-Api-Key':key
+        },
+    },function(error,response,body){
+        if(error) res.json(error);
+        else if(response.statusCode!=200) return console.error('Error:',response.statusCode,body.toString('utf8'));
+        else res.json(body);
+    })
+});
+
+module.exports=quoteRouter;
